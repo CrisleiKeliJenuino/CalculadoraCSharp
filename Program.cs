a86a75ef5fc1df5479cd3428c5622186a7be5adb
CrisleiKeli <crislei.jenuino@etec.sp.gov>
2025-09-12 17:30:09 -0300

    at calc

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalculadoraCSharp.Models;
+
+Calculadora calc = new Calculadora();
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.dividir(2, 2);
